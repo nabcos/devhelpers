2dbf2679364e14b68e31b448a380adc97686e800
Benjamin Hanzelmann <ben@nabcos.de>
2012-08-31 09:50:20 +0200

    o let checkLicense also prompt to do license:format

--- checkLicense.sh
+++ checkLicense.sh
@@ -7,7 +7,34 @@
 # - pom.xml contains a reference to maven-license-plugin
 #
 
+missingHeaders() {
+    mvn license:check -N | grep "Missing header in:"
+    return $?
+}
+
 if egrep -qs '<groupId>com.mycila.maven-license-plugin</groupId>' pom.xml && egrep -qs '<artifactId>maven-license-plugin</artifactId>' pom.xml
 then
-    exec mvn license:check -N
+    if missingHeaders
+    then
+	if [ -e /dev/tty ]
+	then
+	    exec < /dev/tty
+
+	    echo "Missing license headers found! Should we run license:format? (y/n)"
+	    read check
+	    if [ "$check" = "y" ]
+	    then
+		mvn license:format -N
+		if missingHeaders
+		then
+		    return 1
+		fi
+	    fi
+	else
+	    echo -e "Missing license headers found! Run\n  mvn license:format -N\nto add headers."
+	fi
+
+	return 1
+    fi
+
 fi
